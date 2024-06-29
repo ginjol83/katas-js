07346e28db861d7af66fac87f44e8b86a5ec493c
unknown <andresgime@gmail.com>
2024-06-30 00:01:34 +0200

    News 2 exercise added

---
+++ js-katas/summingNumberDigit.js
@@ -0,0 +1,16 @@
+
+/** 
+ * Write a function named sumDigits which takes a number as input and returns 
+ * the sum of the absolute value of each of the number's decimal digits.
+ * For example: (Input --> Output)
+ * 
+ * 10 --> 1
+ * 99 --> 18
+ * -32 --> 5
+ * Let's assume that all numbers in the input will be integer values.
+ */ 
+
+const sumDigits = (number) => Array.from(String(number),Number).reduce((accumulator, currentValue) => accumulator + currentValue)
+
+
+console.log(sumDigits(99))
